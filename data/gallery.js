98cf183f98a4ed904a9f1fc74d854258c398dfb0
kraftkreative <kraftkreative27@gmail.com>
2026-02-13 14:15:19 +0530

    Add files via upload

---
+++ data/gallery.js
@@ -0,0 +1,22 @@
+const GALLERY = {
+
+  Portraits: [
+    "assets/portraits/p1.jpeg",
+    "assets/portraits/p2.jpg"
+  ],
+
+  Weddings: [
+    "assets/wedding/w1.jpeg",
+    
+  ],
+
+  "Love Stories": [
+    "assets/love-stories/l1.jpeg"
+  ],
+
+  Commercial: [
+    "assets/commercials/c1.jpeg",
+    "assets/commercials/c2.jpeg"
+  ]
+
+};
